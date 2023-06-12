cdb8d792dbdaa5af26a98e87bce358cbd52c33f6
zenbooster <zenbooster@gmail.com>
2023-06-12 23:56:23 +0300

    Первый коммит. Работает только с китайским беспроводным джойстиком. Коды клавиш фиксированы.

---
+++ main.py
@@ -0,0 +1,291 @@
+#!/usr/bin/env python3
+#coding: utf-8
+
+# Imports
+import sys
+import pygame as pg
+import random as rnd
+from threading import Timer
+import gtts
+from plasma import csPlasma
+
+class csTabButton:
+    def __init__(self, sc, x, y, w, h):
+        self.sc = sc
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+    
+    def run(self):
+        pass
+
+class csDNB:
+    def timeout(self):
+        if self.running:
+            if self.i_step > self.n:
+                if self.seq_a[0] == self.seq_a[self.n]:
+                    if self.is_a_clicked:
+                        pass
+                    else:
+                        self.i_a_score -= 1
+                if self.seq_b[0] == self.seq_b[self.n]:
+                    if self.is_b_clicked:
+                        pass
+                    else:
+                        self.i_b_score -= 1
+            
+            self.is_a_clicked = False
+            self.is_b_clicked = False
+            self.is_a_released = False
+            self.is_b_released = False
+
+            #self.i_on = rnd.randrange(9)
+            while True:
+                i_on = rnd.randrange(9)
+                if self.i_on != i_on:
+                    self.i_on = i_on
+                    break
+
+            self.i_on_fx = 0
+
+            #text = chr(ord('А') + rnd.randrange(9))
+            while True:
+                text = chr(ord('А') + rnd.randrange(9))
+                if self.text != text:
+                    self.text = text
+                    break
+
+            if self.is_voice:
+                csDNB.saymsg(text)
+
+            if len(self.seq_a) > self.n:
+                self.seq_a.pop(0)
+
+            if len(self.seq_b) > self.n:
+                self.seq_b.pop(0)
+
+            self.seq_a.append(self.i_on)
+            self.seq_b.append(text)
+            
+            if self.is_text_buttons:
+                self.text_surface = self.font.render(text, False, (self.brightness, 0, 0))
+                self.text_w, self.text_h = self.font.size(text)
+
+            self.i_step += 1
+
+            self.t = Timer(2, self.timeout)
+            self.t.start()
+    
+    def resize(self):
+        self.width = width = self.sc.get_width()
+        self.height = height = self.sc.get_height()
+
+        pan_indent = self.pan_indent = self.indent * 2
+        #self.y_butt = y_butt = height - height // 10 - pan_indent
+        #pan_top = self.pan_top = y_butt - pan_indent
+        pan_top = self.pan_top = height - height // 10 - 2*pan_indent
+        self.y_butt = pan_top + pan_indent
+
+        self.w = width // 5
+        self.h = pan_top // 5
+        self.ww = self.w + self.indent
+        self.hh = self.h + self.indent
+
+        pg.font.init()
+        self.font = pg.font.SysFont('arial', self.h // 2)
+
+    def cre8msg(msg, fname):
+        tts = gtts.gTTS(msg, lang = 'ru')
+        tts.save(f'audio/{fname}.mp3')
+
+    def saymsg(fname, vol=1):
+        pg.mixer.music.load(f'audio/{fname}.mp3')
+        pg.mixer.music.set_volume(vol)
+        pg.mixer.music.play()
+
+    def __init__(self):
+        pg.init()
+
+        self.fps = 60
+        self.fpsClock = pg.time.Clock()
+        width, height = 1024, 768
+        sc = self.sc = pg.display.set_mode((width, height), pg.RESIZABLE)
+
+        self.indent = 8
+        self.resize()
+
+        # self.brightness = 128
+        self.brightness = 128*2-1
+        #self.alpha = 0x9f
+        self.alpha = 0x7f
+        self.color_off = (0, 0, self.brightness)
+        self.color_on = (0, self.brightness, 0)
+        self.color_bt_wait = (self.brightness, self.brightness, self.brightness)
+        self.color_bt_ok = (0, self.brightness, 0)
+        self.color_bt_err = (self.brightness, 0, 0)
+
+        self.n = 2
+        self.seq_a = []
+        self.seq_b = []
+
+        #pg.joystick.init()
+        jc = pg.joystick.get_count()
+        print(f'joystick_count: {jc}')
+        self.j = pg.joystick.Joystick(0)
+        self.j.init()
+
+        text = '+' # chr(0x25ac)
+        self.text_a_surface = self.font.render(text, False, (self.brightness, 0, 0))
+        self.text_a_w, self.text_a_h = self.font.size(text)
+        text = 'A'
+        self.text_b_surface = self.font.render(text, False, (self.brightness, 0, 0))
+        self.text_b_w, self.text_b_h = self.font.size(text)
+
+        self.is_a_clicked = False
+        self.is_a_released = True
+        self.is_b_clicked = False
+        self.is_b_released = True
+        self.i_a_score = 0
+        self.i_b_score = 0
+        self.i_step = 0
+
+        self.i_on = 0
+        self.text = 'A'
+        self.i_on_fx = 0
+
+        #self.is_text_buttons = False
+        self.is_text_buttons = True
+        self.is_voice = False
+
+        if self.is_voice:
+            for i in range(9):
+                s = chr(ord('А') + i)
+                csDNB.cre8msg(s, s)
+
+        self.plasma = csPlasma(sc)
+        rnd.seed()
+        self.running = True
+
+    def rect_text(self, sc, c, x, y, w, h, tsc, tw, th):
+        '''
+        if c == self.color_on:
+            if not self.i_on_fx:
+                self.i_on_fx = 255
+            else:
+                if self.i_on_fx > 20:
+                    self.i_on_fx -= 20
+        
+        c = (self.i_on_fx, max(self.i_on_fx, self.brightness), self.i_on_fx)
+        '''
+
+        pg.draw.rect(sc, c, (x, y, w, h))
+        sc.blit(tsc, (x + (w - tw) // 2, y + (h - th) // 2))
+    
+    def run(self):
+        self.timeout()
+
+        # Game loop.
+        while  True:
+            #self.sc.fill((0, 0, 0))
+            self.plasma.run()
+
+            for event in pg.event.get():
+                if event.type == pg.VIDEORESIZE:
+                    self.resize()
+                elif event.type == pg.QUIT:
+                    self.running = False
+                    self.t.cancel()
+                    pg.quit()
+                    sys.exit()
+                elif event.type == pg.JOYBUTTONDOWN:
+                    #print(f'event.button = {event.button}')
+                    if event.button == 9: # start
+                        self.i_a_score = 0
+                        self.i_b_score = 0
+                        self.i_step = 1
+                        self.seq_a = [self.seq_a[-1]]
+                        self.seq_b = [self.seq_b[-1]]
+
+                    elif not self.is_b_released and (event.button == 4):
+                        self.is_b_clicked = True
+                        #self.is_b_released = False
+                        if len(self.seq_b) > self.n:
+                            if self.seq_b[0] == self.seq_b[self.n]:
+                                bt_b_color = self.color_bt_ok
+                                self.i_b_score += 1
+                            else:
+                                bt_b_color = self.color_bt_err
+                                self.i_b_score -= 1
+                    elif not self.is_a_released and (event.button == 5):
+                        self.is_a_clicked = True
+                        #self.is_a_released = False
+                        if len(self.seq_a) > self.n:
+                            if self.seq_a[0] == self.seq_a[self.n]:
+                                bt_a_color = self.color_bt_ok
+                                self.i_a_score += 1
+                            else:
+                                bt_a_color = self.color_bt_err
+                                self.i_a_score -= 1
+                elif event.type == pg.JOYBUTTONUP:
+                    if event.button == 4:
+                        self.is_b_released = True
+                    elif event.button == 5:
+                        self.is_a_released = True
+
+            # Main Loop Code belongs here
+            i = 0
+            sc_tab = pg.Surface((self.w + 2 * self.ww, self.h + 2 * self.hh))
+            for y in range(0, 3 * self.hh, self.hh):
+                for x in range(0, 3 * self.ww, self.ww):
+                    if i == self.i_on:
+                        if self.is_text_buttons:
+                            self.rect_text(sc_tab, self.color_on, x, y, self.w, self.h, self.text_surface, self.text_w, self.text_h)
+                        else:
+                            pg.draw.rect(sc_tab, self.color_on, (x, y, self.w, self.h))
+                    else:
+                        pg.draw.rect(sc_tab, self.color_off, (x, y, self.w, self.h))
+
+                    i += 1
+
+            sc_tab.set_alpha(self.alpha)
+            self.sc.blit(sc_tab, (self.w, self.h))
+            
+            if not self.is_a_clicked:
+                bt_a_color = self.color_bt_wait
+            if not self.is_b_clicked:
+                bt_b_color = self.color_bt_wait
+
+            #self.pan_indent = self.indent * 2
+            #y_butt = self.height - self.h // 2 - self.pan_indent
+            #pan_top = y_butt - self.pan_indent
+            y_butt = self.y_butt
+            pan_top = self.pan_top
+
+            psc = pg.Surface((self.width, self.height - pan_top))
+            pg.draw.rect(psc, (0, 0, 0x30), (0, 0, self.width - 1, self.height - pan_top - 1))
+            y_butt -= pan_top
+            butt_h = self.height - pan_top - 2 * self.pan_indent
+            self.rect_text(psc, bt_b_color, self.w, y_butt, self.w, butt_h, self.text_b_surface, self.text_b_w, self.text_b_h)
+            self.rect_text(psc, bt_a_color, self.w + 2 * self.ww, y_butt, self.w, butt_h, self.text_a_surface, self.text_a_w, self.text_a_h)
+
+            text = f'{self.i_a_score}'
+            c = self.i_a_score >= 0 and (0, self.brightness, 0) or (self.brightness, 0, self.brightness)
+            self.text_a_score_sc = self.font.render(text, False, c)
+            self.text_a_score_w, self.text_a_score_h = self.font.size(text)
+            psc.blit(self.text_a_score_sc, (self.width - 1 - self.text_a_score_w - 10, y_butt + (butt_h - self.text_a_h) // 2))
+
+            text = f'{self.i_b_score}'
+            c = self.i_b_score >= 0 and (0, self.brightness, 0) or (self.brightness, 0, self.brightness)
+            self.text_b_score_sc = self.font.render(text, False, c)
+            self.text_b_score_w, self.text_b_score_h = self.font.size(text)
+            psc.blit(self.text_b_score_sc, (10, y_butt + (butt_h - self.text_a_h) // 2))
+
+            psc.set_alpha(self.alpha)
+            self.sc.blit(psc, (0, pan_top))
+
+            pg.display.flip()
+            self.fpsClock.tick(self.fps)
+
+o = csDNB()
+o.run()
---
+++ plasma.py
@@ -0,0 +1,70 @@
+import pygame as pg
+import math
+
+
+Pi2 = math.pi * 2.0     # Просто константа 2 * Пи для простоты и скорости
+
+class csPlasma:
+    def getInd(self, num):
+        return int((((self.M - 1) * (num % Pi2) / Pi2)))
+
+    def __init__(self, sc):
+        self.sc = sc
+        self.t = 0 # Сдвиг для получения анимации
+
+        self.M = M = 128           # Размер массива с плазмой
+        self.scale = scale = 2               # Масштаб точек для вывода на экран
+
+        self.delta = Pi2 / (M - 1) # Шаг нашего сдвига, на каждом кадре анимации
+
+        self.sintab = sintab = []     # Таблица заранее просчитанных значений синусов
+        self.costab = costab = []     # и косинусов.
+
+                        # Заполняем начальными значениями таблицы sin и cos
+        t = M - 1
+        for i in range(t):
+            sintab.append(math.sin(i * Pi2 / t))
+            costab.append(math.cos(i * Pi2 / t))
+
+    def run(self):
+        M = self.M
+        sintab = self.sintab
+        costab = self.costab
+        scale = self.scale
+        t = self.t
+
+        surf = pg.Surface((M * scale, M * scale))
+        for i in range(M):
+            y = i / M
+            for j in range(M):
+                x = j / M
+                
+                # Вычисляем цветовые коэффициенты и затем сами цветовые составляющие
+                xpt = x + t
+                v_sin = sintab[self.getInd(xpt)]
+                a1 = 8.0 * v_sin
+                a2 = 7.0 * costab[self.getInd(xpt)]
+                a3 = 6.0 * v_sin
+                r = int(100 * abs(sintab[self.getInd(a1 * x + t)] + costab[self.getInd(a1 * y - t)]))
+                g = int(100 * abs(costab[self.getInd(a2 * y - t)] + sintab[self.getInd(a2 * x + t)]))
+                b = int(100 * abs(sintab[self.getInd(a3 * x + t)] + costab[self.getInd(a3 * y - t)]))
+                pg.draw.rect(surf, (r//10, g//10, b//4), (i*scale, j*scale, scale, scale))
+        
+        surf_x = pg.transform.flip(surf, True, False)
+        surf_y = pg.transform.flip(surf, False, True)
+        surf_xy = pg.transform.flip(surf, True, True)
+        m_scale = M * scale
+        for j in range(1 + self.sc.get_height() // m_scale):
+            for i in range(self.sc.get_width() // m_scale):
+                if j & 1:
+                    src = (i & 1) and surf_xy or surf_y
+                else:
+                    src = (i & 1) and surf_x or surf
+
+                self.sc.blit(src, (i * m_scale, j * m_scale))
+
+        # "Двигаем" анимацию
+        t += self.delta
+        if t >= Pi2:
+            t = 0
+        self.t = t
